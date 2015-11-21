b0da4b8c72c128a301f2a1714a8bf2c3e09e15f1
Arvind Sontha <asontha@uci.edu>
2015-11-21 09:14:23 -0800

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name callendarApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the callendarApp
+ */
+angular.module('callendarApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name callendarApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the callendarApp
+ */
+angular.module('callendarApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
